d5ab9430b56e49d6b0652a3eb13a3d751e3265b9
Serega2780 <serega2780@gmail.com>
2019-01-14 08:36:38 +0300

    Monitoring-2 v.1

--- docker/pre-script.sh
+++ docker/pre-script.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 eval $(docker-machine env docker-host)
 docker-machine scp cloudprober.cfg docker-host:/tmp
+docker-machine scp daemon.json docker-host:/tmp
+docker-machine scp telegraf.conf docker-host:/tmp
+docker-machine ssh docker-host sudo cp -r /tmp/daemon.json /etc/docker
 docker-machine ssh docker-host 'sudo sysctl -w net.ipv4.ping_group_range="0 5000"'
+docker-machine ssh docker-host 'sudo systemctl restart docker'
