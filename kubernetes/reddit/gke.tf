62e83605f83b31fd7f6e33500cc1ee19839f5eab
Serega2780 <serega2780@gmail.com>
2019-01-31 16:06:33 +0300

    Kubernetes-5 v.1

--- kubernetes/reddit/gke.tf
+++ kubernetes/reddit/gke.tf
@@ -2,13 +2,28 @@ provider "google" {
   project     = "docker-223805"
   region      = "europe-west1"
 }
+
 resource "google_container_cluster" "primary" {
   name               = "cluster-1"
   zone               = "europe-west1-d"
-  initial_node_count = 2
-  node_config {
-    machine_type = "g1-small"
-    disk_size_gb = 20
+  
+  enable_legacy_abac = true
+
+  node_pool {
+    name = "my-node-pool-1"
+    node_count = 2
+    node_config {
+      machine_type = "g1-small"
+      disk_size_gb = 20
+    }
+  }
+  node_pool {
+    name = "my-node-pool-2"
+    node_count = 1
+    node_config {
+      machine_type = "n1-standard-2"
+      disk_size_gb = 40
+    }
   }
   master_auth {
     username = ""
